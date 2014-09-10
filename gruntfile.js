549e9819c8ae580229e79fcfc39fc1fd87676969
Jens Melgaard <admin@it-links.dk>
2014-09-10 21:04:25 +0200

    removed unused grunt stuff

--- gruntfile.js
+++ gruntfile.js
@@ -1,66 +1,20 @@
 ﻿/*global module:false*/
 module.exports = function (grunt) {
     "use strict";
-
-    //var files = require("./dev/files").files;
-
-    // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         banner: '',
-        clean: {
-            build: {
-                src: ['artifacts']
-            }
-        },
-
-        concat: {
-            options: {
-                banner: '<%= banner %>',
-                stripBanners: true
-            },
-            core: {
-                src: "artifacts/src/**./*.js",
-                dest: 'build/<%= pkg.name %>.js'
-            }
-        },
-
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            core: {
-                src: '<%= concat.core.dest %>',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
-
-        watch: {
-            dev: {
-                files: ['src/**/*.ts'],
-                tasks: ['build']
-            }
-        },
-
         mochaTest: {
             test: {
                 src: ['test/**/*.spec.js']
             }
         }
-
     });
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-mocha-test');
 
     // Default task.
-    grunt.registerTask('default', ['clean']);
     grunt.registerTask('test', ['mochaTest']);
-
+    grunt.registerTask('default', ['test']);
 };
